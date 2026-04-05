eda18ed6786ed940c89a0ea4b503c7c424889a92
mozhaa <mozhay2005@gmail.com>
2026-04-05 19:02:39 +0300

    fix!: avoid using bare hashed_id()

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from .execute_python import ExecutePython
 
 from machineid import hashed_id
-id_ = hashed_id()
+id_ = hashed_id("ComfyUI-Execute-Python")
 
 NODE_CLASS_MAPPINGS = {f"ExecutePython{id_}": ExecutePython}
 NODE_DISPLAY_NAME_MAPPINGS = {f"ExecutePython{id_}": "Execute Python"}
